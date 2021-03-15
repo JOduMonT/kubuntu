61a8f79f32b114c7d1a7edd8788060a0833a7165
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-03-15 12:28:04 +0000

    Create brave.sh

---
+++ ppa/brave.sh
@@ -0,0 +1,4 @@
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com stable main" >> /etc/apt/sources.list.d/brave.list'
+sudo apt update
+sudo apt install -y brave-browser
