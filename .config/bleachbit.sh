baffb847777932b7f96f3f1790fe05d799a254ce
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-03-15 12:40:29 +0000

    mv

--- .config/bleachbit.sh
+++
@@ -1,5 +0,0 @@
-### bleachit
-sudo apt install -y bleachbit
-mkdir $HOME/.config/bleachbit
-wget -O $HOME/.config/bleachbit/bleachbit.ini https://gitlab.com/garuda-linux/themes-and-settings/settings/garuda-common-settings/-/raw/master/etc/skel/.config/bleachbit/bleachbit.ini
-sudo cp -r $HOME/.config/bleachbit /etc/skel/.config/
