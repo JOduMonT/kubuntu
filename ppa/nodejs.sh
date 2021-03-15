dcbec116df37eb91f88fe7ec305196d2e289c10c
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-03-15 12:27:25 +0000

    Create nodejs.sh

---
+++ ppa/nodejs.sh
@@ -0,0 +1,5 @@
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+sudo sh -c "echo deb https://deb.nodesource.com/node_14.x focal main \
+> /etc/apt/sources.list.d/nodesource.list"
+sudo apt update
+sudo apt install -y nodejs
