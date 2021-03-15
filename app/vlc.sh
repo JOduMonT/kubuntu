744ea501b67497cad2b0bc94314bd6faf401a06f
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-03-15 12:40:55 +0000

    Create vlc.sh

---
+++ app/vlc.sh
@@ -0,0 +1,7 @@
+### vlc
+sudo apt install -y vlc
+mkdir $HOME/.config/vlc
+wget -O $HOME/.config/vlc/vlcrc https://gitlab.com/garuda-linux/themes-and-settings/settings/garuda-common-settings/-/raw/master/etc/skel/.config/vlc/vlcrc
+sudo cp -r $HOME/.config/vlc /etc/skel/.config/
+
+
