72c32e2fe08ba710bf0e3dff6bddb0189c83cf88
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-03-15 12:28:23 +0000

    Create obs.sh

---
+++ ppa/obs.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository -y ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install -y obs-studio
+
+wget https://github.com/Palakis/obs-ndi/releases/download/4.9.1/libndi4_4.5.1-1_amd64.deb
+sudo dpkg -i libndi*
