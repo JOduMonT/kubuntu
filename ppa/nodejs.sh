d1498e3e9e89b7b7ad5c7d80927e4ff1f44493ab
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-05-03 03:03:02 -0400

    Update nodejs.sh

--- ppa/nodejs.sh
+++ ppa/nodejs.sh
@@ -1,3 +1,4 @@
+sudo apt install -y curl
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 sudo sh -c "echo deb https://deb.nodesource.com/node_14.x focal main \
 > /etc/apt/sources.list.d/nodesource.list"
