30631988374be8b65ff1bb88a48926ec3df9a6d9
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-05-07 04:16:51 -0400

    update npm

--- ppa/nodejs.sh
+++ ppa/nodejs.sh
@@ -4,3 +4,4 @@ sudo sh -c "echo deb https://deb.nodesource.com/node_14.x focal main \
 > /etc/apt/sources.list.d/nodesource.list"
 sudo apt update
 sudo apt install -y nodejs
+sudo npm install -g npm
