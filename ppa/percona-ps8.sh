bd236f32cb30359edcaacb186078211c376b8746
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-05-18 11:59:35 +0700

    Update percona-ps8.sh

--- ppa/percona-ps8.sh
+++ ppa/percona-ps8.sh
@@ -1,6 +1,5 @@
-apt install -y gnupg2
-wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
-dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
-apt update
-percona-release setup ps80
-apt install -y percona-server-server
+sudo apt install -y gnupg2
+wget -O - https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb | sudo dpkg -i
+sudo apt update
+sudo percona-release setup ps80
+sudo apt install -y percona-server-server
