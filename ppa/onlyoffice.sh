e6c644857832fd4d139cb3388d608b7b69eb0664
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-05-18 11:58:29 +0700

    Update onlyoffice.sh

--- ppa/onlyoffice.sh
+++ ppa/onlyoffice.sh
@@ -1,4 +1,4 @@
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
-echo "deb https://download.onlyoffice.com/repo/debian squeeze main" > /etc/apt/sources.list.d/onlyoffice.list
-apt update
-apt install -y onlyoffice-desktopeditors
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+echo "deb https://download.onlyoffice.com/repo/debian squeeze main" | sudo tee /etc/apt/sources.list.d/onlyoffice.list
+sudo apt update
+sudo apt install -y onlyoffice-desktopeditors
