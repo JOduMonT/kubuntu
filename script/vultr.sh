4a8e47a62c09276b0e8ee76d5788cb4cf499688f
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-05-19 09:16:20 +0700

    Update vultr.sh

--- script/vultr.sh
+++ script/vultr.sh
@@ -3,11 +3,12 @@
 apt list --installed >> installed.origin
 free -h > mem.origin
 
+add-apt-repository universe
 apt update
 
 apt purge -y alsa* bolt btrfs* containerd crda cryptsetup* docker.io dmsetup iw lvm2 lxd* mdadm ntfs-3g open-iscsi sg3* snapd sound-theme* runc telnet thin-provisioning* ubuntu-advantage* wireless* xfsprogs
 
-apt install -y apt-transport-https ca-certificates curl gnupg intel-microcode lsb-release
+apt install -y curl intel-microcode
 
 apt autoremove --purge -y
 
