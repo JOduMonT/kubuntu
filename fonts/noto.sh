bcf4d3cb6ec6c6e017f5a66ae00a4da9798bccda
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-06-11 13:29:38 +0700

    Update noto.sh

--- fonts/noto.sh
+++ fonts/noto.sh
@@ -6,6 +6,12 @@ wget -O /tmp/Noto$file.zip https://fonts.google.com/download?family=Noto%20$file
 unzip -o /tmp/Noto$file.zip -d /tmp
 done
 
+for file in ColorEmoji Emoji
+do
+wget -O /tmp/Noto$file.zip https://noto-website-2.storage.googleapis.com/pkgs/Noto$file-unhinted.zip
+unzip -o /tmp/Noto$file.zip -d /tmp
+done
+
 mv /tmp/Noto*.ttf ~/.local/share/fonts/
 
 fc-cache -fv
