4c56d39845e36053e7bc22e7e756fb6303d2a08f
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-08-19 11:53:36 +0700

    Create litespeed-nodejs.sh

---
+++ app/litespeed-nodejs.sh
@@ -0,0 +1,3 @@
+bash <(curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Setup/nodejssetup.sh)
+bash <(curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh)
+reboot
