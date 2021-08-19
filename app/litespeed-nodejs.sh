78be4b1d74a86f1152326a094d9bce38d740914b
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-08-19 13:08:26 +0700

    Update litespeed-nodejs.sh

--- app/litespeed-nodejs.sh
+++ app/litespeed-nodejs.sh
@@ -1,3 +1,8 @@
 bash <(curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Setup/nodejssetup.sh)
 bash <(curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh)
+
+apt install -y ufw
+ufw allow 22,53,80,443,7080,8088/tcp
+ufw default reject
+echo "y" > ufw enable
 reboot
