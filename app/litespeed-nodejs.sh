62876f713ccfc3518b942c9c0289a83abed655b4
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-08-20 10:30:05 +0700

    Update litespeed-nodejs.sh

--- app/litespeed-nodejs.sh
+++ app/litespeed-nodejs.sh
@@ -2,7 +2,11 @@ bash <(curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/m
 bash <(curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh)
 
 apt install -y ufw
-ufw allow 22,53,80,443,7080,8088/tcp
+ufw allow 443 # need udp for QUIC https/3
+ufw allow 22/tcp
+ufw allow 53/tcp
+ufw allow 80/tcp
+ufw allow 7080/tcp
 ufw default reject
 echo "y" > ufw enable
 reboot
