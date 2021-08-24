245badd8427db320cab0e3938703a21616312d0f
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-08-24 04:43:15 -0400

    Update obs.sh

--- ppa/obs.sh
+++ ppa/obs.sh
@@ -1,6 +1,9 @@
+OBS_VS="4.9.1"
+NDI_VS="4_4.5.1-1"
+
 sudo add-apt-repository -y ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install -y obs-studio
 
-wget https://github.com/Palakis/obs-ndi/releases/download/4.9.1/libndi4_4.5.1-1_amd64.deb
+wget https://github.com/Palakis/obs-ndi/releases/download/$OBS_VS/libndi$NDI_VS_amd64.deb
 sudo dpkg -i libndi*
