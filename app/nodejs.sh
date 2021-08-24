f76dfabc18bc1c504ed38abaff3d337db37ca413
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-08-25 01:21:27 +0700

    Update nodejs.sh

--- app/nodejs.sh
+++ app/nodejs.sh
@@ -1,6 +1,7 @@
+NODE_VS="14"
 sudo apt install -y curl
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-sudo sh -c "echo deb https://deb.nodesource.com/node_14.x focal main \
+sudo sh -c "echo deb https://deb.nodesource.com/node_$NODE_VS.x focal main \
 > /etc/apt/sources.list.d/nodesource.list"
 sudo apt update
 sudo apt install -y nodejs
