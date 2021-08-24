384233e36e9eda517652612f3b600d8709a08ab9
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-08-24 04:41:42 -0400

    Update rambox.sh

--- app/rambox.sh
+++ app/rambox.sh
@@ -1,2 +1,3 @@
-wget -O /tmp/rambox-ce.deb https://github.com/$(curl -s https://github.com/ramboxapp/community-edition/releases/tag/0.7.7|grep -m1 amd64.deb|cut -d\" -f2)
+RAMBOX_VS="0.7.9"
+wget -O /tmp/rambox-ce.deb https://github.com/$(curl -s https://github.com/ramboxapp/community-edition/releases/tag/$RAMBOX_VS|grep -m1 amd64.deb|cut -d\" -f2)
 sudo dpkg -i /tmp/rambox-ce.deb
