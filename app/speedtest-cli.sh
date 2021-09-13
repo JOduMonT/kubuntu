a8d7f9834f4373d81e3f1ebd55c122d5408d0567
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-09-13 07:47:36 +0700

    Update speedtest-cli.sh

--- app/speedtest-cli.sh
+++ app/speedtest-cli.sh
@@ -1,1 +1,6 @@
+sudo ln -s /usr/bin/python3 /usr/bin/python
 
+wget -O /tmp/speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x /tmp/speedtest-cli
+
+/tmp/speedtest-cli
