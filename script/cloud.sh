98bc50cc34cc4e87d329b839ec783ba203d957bf
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-09-21 08:24:53 +0700

    Update cloud.sh

--- script/cloud.sh
+++ script/cloud.sh
@@ -40,19 +40,3 @@ for config in cpu-governor energy_performance_preference pcie_aspm_performance p
 done
 
 echo 'fs.inotify.max_user_watches=524288' > /etc/sysctl.d/99-fs_inotify.conf
-
-## CPU Vulnerability Scan
-wget https://raw.githubusercontent.com/speed47/spectre-meltdown-checker/master/spectre-meltdown-checker.sh
-bash spectre-meltdown-checker.sh >> cpuvuln.log
-
-## SpeedTest
-wget https://raw.github.com/blackdotsh/curl-speedtest/master/speedtest.sh
-bash speedtest.sh >> netspeed.log
-
-## HardDrive Test
-dd if=/dev/zero of=/tmp/test1.img bs=1M count=1024 oflag=dsync 2>> hdwrite.log
-dd if=/dev/zero of=/tmp/test2.img bs=512 count=1000 oflag=dsync 2>> hdlatency.log
-hdparm -t /dev/${lsblk|grep /$|cut -d" " -f1} >> hdparam.log
-
-#apt install -y apache2-utils
-#ab -n 10000 -k -H "Accept-Encoding: gzip,deflate" -c 100 https://
