6651c532d2edaeeae01ac1eadf784dce0025219a
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2021-09-21 08:21:01 +0700

    Update docker.sh

--- app/docker.sh
+++ app/docker.sh
@@ -1,2 +1,4 @@
 curl -sSL https://get.docker.com/ | CHANNEL=stable sh
 systemctl enable --now docker
+curl -L https://github.com/docker/compose/releases/download/$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
